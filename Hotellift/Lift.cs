77e2a69d9b3af336a802939c6b0f19b78d23ca33
Nagy Martin Márk <nagy.martin.mark@mechwart.local>
2023-11-14 12:18:55 +0100

    commit

---
+++ Hotellift/Lift.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotellift
+{
+    internal class Lift
+    {
+        string usageTime;
+        int cardID;
+        int startingID;
+        int endID;
+
+        public Lift(string usageTime, int cardID, int startingID, int endID)
+        {
+            this.usageTime = usageTime;
+            this.cardID = cardID;
+            this.startingID = startingID;
+            this.endID = endID;
+        }
+
+        public string UsageTime { get => usageTime; }
+        public int CardID { get => cardID;}
+        public int StartingID { get => startingID;}
+        public int EndID { get => endID;}
+    }
+}
---
+++ Hotellift/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Hotellift
+{
+    internal class Program
+    {
+        static List<Lift> liftList = new List<Lift>();
+        static void Main(string[] args)
+        {
+            StreamReader sr = new("lift.txt");
+            while (!(sr.EndOfStream))
+            {
+                string[] line = sr.ReadLine().Split(" ");
+                Lift newLift = new Lift(line[0], Convert.ToInt32(line[1]), Convert.ToInt32(line[2]), Convert.ToInt32(line[3]));
+                liftList.Add(newLift);
+            }
+
+            Console.WriteLine($"3. feladat: {liftList.Count}");
+
+            Console.WriteLine($"5. feladat: {liftList.Max(x => x.EndID)}");
+            Console.WriteLine("6. feladat:");
+
+            int searchedCards;
+            int searchedEnd;
+
+            try
+            {
+                Console.WriteLine("Kárya száma: ");
+                searchedCards = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Célszint száma: ");
+                searchedEnd = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Számot!");
+                throw;
+            }
+
+            
+            Console.WriteLine("7. feladat:");
+
+            foreach (var item in liftList)
+            {
+                if (item.CardID == searchedCards && item.EndID == searchedEnd) {
+                    Console.WriteLine("Utaztak");
+                    break;
+                }
+            }
+
+            //A 8. feladatot nemtudom megoldani.
+        }
+    }
+}
